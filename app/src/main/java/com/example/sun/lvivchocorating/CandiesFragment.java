cabe0aaa5ea47066b78e45b08cb0ef444139f3cd
Sundarina <sundarina@ukr.net>
2017-08-28 22:59:49 +0300

    category (array)
    нажатие на боковой панели ведет к фрагменту со списком категорий

--- app/src/main/java/com/example/sun/lvivchocorating/CandiesFragment.java
+++ app/src/main/java/com/example/sun/lvivchocorating/CandiesFragment.java
@@ -3,20 +3,15 @@ package com.example.sun.lvivchocorating;
 
 import android.app.ListFragment;
 import android.content.Context;
-import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteException;
-import android.database.sqlite.SQLiteOpenHelper;
+
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CursorAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
-import android.widget.Toast;
+
 
 
 /**
@@ -24,9 +19,6 @@ import android.widget.Toast;
  */
 public class CandiesFragment extends ListFragment {
 
-
-    private SQLiteDatabase db;
-    private Cursor cursor;
 
     interface CandyListListener {
         void itemClicked(long id);
@@ -45,29 +37,10 @@ public class CandiesFragment extends ListFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ListView listCandy = getListView();
 
-        try {
-            SQLiteOpenHelper candyDatabaseHelper = new ChocoDatabaseHelper(getActivity());
-            //Получить ссылку на базу данных.
-            db = candyDatabaseHelper.getReadableDatabase();
-            //Создать курсор
-            cursor = db.query("CANDY", new String[]{"_id", "CATEGORY"}, null, null, null, null, null);
-            //Создать адаптер курсора.
-            //Связать содержимое столбца CATEGORY с текстом в ListView
-            CursorAdapter listAdapter = new SimpleCursorAdapter(getActivity(), android.R.layout.simple_list_item_1, cursor, new String[]{"CATEGORY"}, new int[]{android.R.id.text1}, 0);
-            listCandy.setAdapter(listAdapter);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(inflater.getContext(), android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.category));
+        setListAdapter(adapter);
 
-        } catch (SQLiteException e) {
-            //Вывести сообщение для пользователя, если будет выдано исключение SQLiteException.
-            Toast toast = Toast.makeText(getActivity(), "Database is unavaliable", Toast.LENGTH_SHORT);
-            toast.show();
-
-//            //адаптер массива
-//            //получение контекста от инфлатера
-//            ArrayAdapter<String> adapter = new ArrayAdapter<String>(inflater.getContext(), android.R.layout.simple_list_item_1, names);
-//            setListAdapter(adapter);
-        }
         //связать адаптер массива со списковым представлением
         //Вызов метода onCreateView() суперкласса предоставляет макет по умолчанию для ListFragment
         return super.onCreateView(inflater, container, savedInstanceState);
@@ -81,28 +54,13 @@ public class CandiesFragment extends ListFragment {
         this.listener = (CandyListListener) context;
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        cursor.close();
-        db.close();
-    }
-
-
-    //переписать
-    //
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         if (listener != null) {
-            //  Сообщить слушателю о то м, что
-            //  на одном из вариантов ListView  был сделан щелчок
-                    listener.itemClicked(id);
-
-            Intent intent = new Intent(getActivity(), CandyActivity.class);
-            intent.putExtra(CandyActivity.EXTRA_CANDYNO, (int) id);
-            startActivity(intent);
+//              Сообщить слушателю о то м, что
+//              на одном из вариантов ListView  был сделан щелчок
+            listener.itemClicked(id);
         }
     }
-
 }
